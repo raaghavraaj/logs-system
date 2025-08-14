0af175d7478d479bd63219d7651c53e1fffe4d19
Raaghav Raaj <raa.raaj45@gmail.com>
2025-08-15 02:31:53 +0530

    Adding boilerplate code for logs-distributor

---
+++ logs-distributor/src/main/java/com/resolveai/logsdistributor/LogsDistributorApplication.java
@@ -0,0 +1,13 @@
+package com.resolveai.logsdistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsDistributorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsDistributorApplication.class, args);
+    }
+
+}
---
+++ logs-distributor/src/test/java/com/resolveai/logsdistributor/LogsDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolveai.logsdistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsDistributorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
