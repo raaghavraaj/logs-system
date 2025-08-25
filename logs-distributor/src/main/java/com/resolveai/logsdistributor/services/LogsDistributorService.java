d2d69c65d238d07d2a5f59c29eeb6cb8b4afaa46
Raaghav Raaj <50041280+raaghavraaj@users.noreply.github.com>
2025-08-25 09:30:30 +0530

    Delete logs-distributor directory

--- logs-distributor/src/main/java/com/resolveai/logsdistributor/services/LogsDistributorService.java
+++
@@ -1,7 +0,0 @@
-package com.resolveai.logsdistributor.services;
-
-import com.resolveai.logsdistributor.models.LogPacket;
-
-public interface LogsDistributorService {
-    void distributePacket(LogPacket packet);
-}
